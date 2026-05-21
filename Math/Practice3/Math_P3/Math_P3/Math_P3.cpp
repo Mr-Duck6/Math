554ea6b73b178eab1d73b001e2a19ea74057d0d1
Mr_Duck6 <datmrduck39@gmail.com>
2026-05-21 03:27:25 +0300

    Add files via upload

---
+++ Math/Practice3/Math_P3/Math_P3/Math_P3.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <limits> 
+
+using namespace std;
+
+int main()
+{
+    int shots = 0;
+    double frequency = 0.0;
+
+    cout << "Enter shots";
+    while (true)
+    {
+        if (cin >> shots && shots >= 0)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Try again";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
+
+    cout << "Enter frequency";
+    while (true)
+    {
+        if (cin >> frequency && frequency >= 0.0 && frequency <= 1.0)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Try again";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
+
+    int hits = static_cast<int>(frequency * shots);
+
+    cout << "\nResult Hits / Shots: " << hits << " / " << shots << endl;
+
+    return 0;
+}
