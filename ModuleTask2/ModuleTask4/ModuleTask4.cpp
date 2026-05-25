993c29b6bbb6784dbc5731943c3e4bddb9c55fe5
Mr_Duck6 <datmrduck39@gmail.com>
2026-05-25 20:19:38 +0300

    Add files via upload

---
+++ ModuleTask2/ModuleTask4/ModuleTask4.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+
+class ClassCounter 
+{
+private:
+    int minval; 
+    int maxval;
+    int current;
+
+public:
+
+    ClassCounter(int min_v, int max_v)
+    {
+        minval = min_v;
+        maxval = max_v;
+        current = min_v;
+    }
+
+    void funcIncrement()
+    {
+        if (current >= maxval)
+        {
+            current = minval;
+        }
+        else 
+        {
+            current++;
+        }
+    }
+
+    int funcGetValue() 
+    {
+        return current;
+    }
+};
+
+int main()
+{
+    ClassCounter obj(1, 3);
+
+    cout << obj.funcGetValue() << endl; 
+
+    obj.funcIncrement();
+    cout << obj.funcGetValue() << endl; 
+
+    obj.funcIncrement();
+    cout << obj.funcGetValue() << endl;
+
+    obj.funcIncrement();
+    cout << obj.funcGetValue() << endl;
+
+    return 0;
+}
